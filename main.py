74dda89ac580194b6a59fc314e8626f838a149be
Sourabh Singh Rawat <s.rawat3.142@live.in>
2025-04-14 00:38:46 +0530

    Add screenshot for sample run output

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
 load_dotenv()
+
+print("Python verson:", sys.version)
 
 client = genai.Client(api_key=os.getenv("API_KEY"))
 
 res = client.models.generate_content(
     model="gemini-2.0-flash",
     contents=[
-        {"role": "user", "parts": [{"text": "who are you"}]}
+        {"role": "user", "parts": [{"text": "who are you?"}]}
     ],
     config={
         'candidate_count': 1,
         'temperature': 0.2,
     })
 
-print(res.to_json_dict())
 print(res.text)
